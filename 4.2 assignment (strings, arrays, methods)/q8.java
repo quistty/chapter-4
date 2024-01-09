6904bb78273b37e786daa2803105a79ca10b7f61
Porsha <78366933+quistty@users.noreply.github.com>
2024-01-08 22:56:51 -0500

    dont get raped

--- 4.2 assignment (strings, arrays, methods)/q8.java
+++ 4.2 assignment (strings, arrays, methods)/q8.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
 
 public class q8 {
-    public static void main(String[] args) {
+    public static void divisibleBySeven(int[] passedArray) {
+        for (int i = 0; i < passedArray.length; i++) {
+            if ((passedArray[i] % 7) == 0) {
+                System.out.print(passedArray[i] + " ");
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+        divisibleBySeven(intArray);
     }
 }

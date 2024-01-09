ff1d48d24e938aba6983b3486bae97d00fe9f78e
Porsha <78366933+quistty@users.noreply.github.com>
2024-01-09 16:39:08 -0500

    uploaded my mistakes!!

--- 4.2 assignment (strings, arrays, methods)/q12.java
+++ 4.2 assignment (strings, arrays, methods)/q12.java
@@ -5,21 +5,30 @@ public class q12 {
     public static boolean echecker(String[] array) {
         String stringChecker;
         boolean hasE = false;
+        int counter = 0;
+        boolean counterTrueOrFalse;
         for (int i = 0; i < array.length; i++) { // while i is less than the length of the array, make the stringchecker
-                                                 // equal to the i equvalent in the array
+                                                 // equal to the string with the same index as i
             stringChecker = array[i];
-            for (int j = 0; j < stringChecker.length(); j++) {
+            counterTrueOrFalse = false;
+            for (int j = 0; j < stringChecker.length(); j++) { // gets the string
+                System.out.println(counter + ": What is this");
+                System.out.println(counterTrueOrFalse + ": counter true or false");
                 if (stringChecker.charAt(j) == 'e') {
-                    hasE = true;
-                    break;
+                    System.out.println("AAAAAAAAAAAAAAAAAA");
+                    counter++;
+                    counterTrueOrFalse = true;
+                }
+                if ((j == stringChecker.length() - 1) && (counter == 0)) {
+                    counterTrueOrFalse = false;
+                }
+                if (counterTrueOrFalse == true) { // doesnt work bc if u have a word with more than 1 e, it adds 1
+                    return true;
+                }
+                if (counterTrueOrFalse == false) {
+                    return false;
                 }
             }
-        }
-
-        if (!hasE) {
-            return false;
-        } else {
-            return true;
         }
 
     }
@@ -29,7 +38,7 @@ public class q12 {
         String[] array = { "bobbie", "nobb", "black uncanny", "defenestration",
                 "pneumonoultramicroscopicsilicovolcanoconiosis" };
         System.out.println(echecker(array)); // true
-        String[] array2 = { "bob", "nobb", "black uncanny" };
+        String[] array2 = { "bobe", "nobbe", "black uncannye" };
         System.out.println(echecker(array2)); // false
         in.close();
     }

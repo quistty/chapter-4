d23d80b756eaad05d240b68889d2ea8632961e59
Porsha <78366933+quistty@users.noreply.github.com>
2024-09-10 10:50:19 -0400

    gpt the best

--- new guy game/game.java
+++ new guy game/game.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.random.*;
+import java.util.Random;
 public class game {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);

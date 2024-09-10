8fa06a5069ebfe0e3c84c6293411d9adf8cbe7ac
Porsha <78366933+quistty@users.noreply.github.com>
2024-09-10 10:21:10 -0400

    bomboclaat

---
+++ new guy game/game.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.random;
+public class game {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int points = 500;
+        whille (userInput != -1) {
+            System.out.println("Enter how much you want to wager");
+            int wager = in.nextInt;
+            if (wager > points){
+                System.out.println("Blud you do not have enough money, don't leeave ur momns in the hood or this yute will be smoked");
+                break;
+            } else {
+                System.outprintln("ok good wager");
+                break;
+            }
+        } 
+        in.close();
+    }
+
+}
